f5d2112cdfa87b0a60c3fd86d9328cd84795919f
Sonia Pepe <sonia.pepe@fdvsolutions.com>
2016-03-14 18:02:34 -0300

    Saving progress, created inputs and add button to create new rows.

--- scripts.js
+++ scripts.js
@@ -25,3 +25,52 @@ var blog2 = new Blog({
 
 //Instantiate a Collection
 var blogs = new Blogs([blog1,blog2]);
+
+//Backbone View for one blog
+var BlogView = Backbone.View.extend({
+	model: new Blog(),
+	//as we have no definition for $el, Backbone will take tagName as the default $el
+	tagName: 'tr',
+
+	initialize : function() {
+		this.template = _.template($('.blogs-list-template').html())
+	},
+
+	render : function() {
+		this.$el.html(this.template(this.model.toJSON()))
+		return this;
+	}
+});
+
+//Backbone View for all blogs
+var BlogsView = Backbone.View.extend({
+	model: blogs,
+	el: $('.blogs-list'),
+
+	initialize: function() {
+		this.model.on('add', this.render, this);
+	},
+
+	render: function() {
+		var self = this;
+		this.$el.html('');
+		_.each(this.model.toArray(), function(blog) {
+			self.$el.append((new BlogView({model: blog})).render().$el);
+		});
+		return this;
+	}
+});
+
+var blogsView = new BlogsView();
+
+$(document).ready(function() {
+	$('.add-blog').on('click', function() {
+		var blog = new Blog({
+			author: $('.author-input').val(),
+			title: $('.title-input').val(),
+			url: $('.url-input').val()
+		});
+		console.log(blog.toJSON());
+		blogs.add(blog);
+	});
+});
