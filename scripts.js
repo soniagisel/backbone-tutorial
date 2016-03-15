f73379e4b94379ebdde47e57934f0f571777d471
Sonia Pepe <sonia.pepe@fdvsolutions.com>
2016-03-15 15:56:01 -0300

    Added delete, edit, cancel and update events to the blog view.

--- scripts.js
+++ scripts.js
@@ -11,27 +11,46 @@ var Blog = Backbone.Model.extend({
 var Blogs = Backbone.Collection.extend({});
 
 //Instantiate two blogs
-var blog1 = new Blog({
-	author: 'Damian',
-	title: 'Damian\'s Blog',
-	url: 'http://damiansblog.com'
-});
-
-var blog2 = new Blog({
-	author: 'Sonia',
-	title: 'Sonia\'s Blog',
-	url: 'http://soniasblog.com'
-});
 
 //Instantiate a Collection
-var blogs = new Blogs([blog1,blog2]);
+var blogs = new Blogs();
 
 //Backbone View for one blog
 var BlogView = Backbone.View.extend({
 	model: new Blog(),
 	//as we have no definition for $el, Backbone will take tagName as the default $el
 	tagName: 'tr',
+	events: {
+		'click .edit-blog'   : 'edit',
+		'click .update-blog' : 'update',
+		'click .cancel'      : 'cancel',
+		'click .delete-blog' : 'delete'
+	},
+	edit: function() {
+		$('.edit-blog').hide();
+		$('.delete-blog').hide();
+		this.$('.update-blog').show();
+		this.$('.cancel').show();
 
+		var author = this.$('.author').html();
+		var title = this.$('.title').html();
+		var url = this.$('.url').html();
+
+		this.$('.author').html('<input type="text" class="form-control author-update" value="' + author + '">');
+		this.$('.title').html('<input type="text" class="form-control title-update" value="' + title + '">');
+		this.$('.url').html('<input type="text" class="form-control url-update" value="' + url + '">');
+	},
+	update: function() {
+		this.model.set('author', $('.author-update').val());
+		this.model.set('title', $('.title-update').val());	
+		this.model.set('url', $('.url-update').val());		
+	},
+	cancel: function() {
+		blogsView.render();
+	},
+	delete: function() {
+		this.model.destroy();
+	},
 	initialize : function() {
 		this.template = _.template($('.blogs-list-template').html())
 	},
@@ -48,7 +67,15 @@ var BlogsView = Backbone.View.extend({
 	el: $('.blogs-list'),
 
 	initialize: function() {
+		var self = this;
 		this.model.on('add', this.render, this);
+		this.model.on('change', function() {
+			setTimeout(function() {
+				self.render();
+			}, 30);
+		}, this);
+
+		this.model.on('remove', this.render, this);
 	},
 
 	render: function() {
